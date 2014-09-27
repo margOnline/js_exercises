aee49898037f2908d579726afb185169723e5675
margOnline <margo@margonline.co.uk>
2014-09-27 21:18:50 +0100

    update script to add input color to favorite

--- colourPicker/script.js
+++ colourPicker/script.js
@@ -1,9 +1,21 @@
-$(document).on('keypress keyup keydown', '#color', function(){
-	var color = $('input').val();
-	setPreviewColor(color);
+$(document).ready(function() {
+	$(document).on('click', '#add-to-favorite', function(){
+		var color = $('#color').val();
+		addBox(color);
+	});
+
+	$(document).on('keypress keyup keydown', '#color', function(){
+		var color = $('input').val();
+		setPreviewColor(color);
+	});
 });
 
 function setPreviewColor(color){
 	$('.preview').css('background-color', color);
 	$('.color-code').text($('.preview').css('background-color'));
+}
+
+function addBox(color) {
+	var fav = "<div class='item' style='background-color: " + color + ";'></div>";
+	$('#colors').prepend(fav);
 }
