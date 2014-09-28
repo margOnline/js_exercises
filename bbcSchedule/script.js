bbf18012acf3e0f4c75f8d0649ac4cae8592447d
margOnline <margo@margonline.co.uk>
2014-09-28 23:10:32 +0100

    show tomorrows programmes when clicking on a genre

--- bbcSchedule/script.js
+++ bbcSchedule/script.js
@@ -13,10 +13,52 @@ function retrieveGenres() {
 }
 
 function displayGenre(genre) {
-	listItem = "<li>" + genre.key + "</li>"
+	listItem = "<li class='js-genre'>" + genre.key + "</li>"
 	$('#genres').append(listItem);
 }
 
+function getTomorrowsSchedule(genre) {
+	$.ajax({
+		url: "http://www.bbc.co.uk/tv/programmes/genres/"+genre+"/schedules/tomorrow.json",
+		dataType: 'json',
+		beforeSend: function(){
+			$('#programmes').empty();
+			$('#programmes').append("<div class='spinner'><img src='spinner.gif' /></div>");
+		}
+	}).done(function(data){
+		$('.spinner').remove();
+		console.log(data);
+		$.each(data.broadcasts, function(index, elem) {
+			displayProgramme(elem);
+		});
+	}).fail(function(){
+		$('#genres').html('There are no programmes');
+	});
+}
+
+function displayProgramme(item){
+	item_html = "<li>";
+	item_html += "<h2>" + item.programme.display_titles.title + "</h2>";
+	item_html += "<p>" + item.programme.short_synopsis + "</p>"; 
+	if (item.programme.image){
+		src = "http://ichef.bbci.co.uk/images/ic/272x153/" + item.programme.image.pid + ".jpg";
+		item_html += "<img src='" + src + "' />";
+	} else {
+		item_html += "<img src='http://placehold.it/272x153' />";
+	}
+	item_html += "<p>Start: " + item.start + " - " + item.end + "</p>";
+	item_html += "<p>Duration: " + item.duration / 60 + " minutes</p>";
+	item_html += "<p class='service'>" + item.service.title + "</p>"
+	item_html += "</li>";
+	$('#programmes').append(item_html);
+}
+
 $(document).ready(function(){
+	
+	$(document).on('click','#genres li', function(e){
+		genre = $(this).text();
+		getTomorrowsSchedule(genre);
+	});
+
 	retrieveGenres();
 });
